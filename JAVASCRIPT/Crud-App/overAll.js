7201e1899ee07ef6e2953d0f66829587083fce12
anurag nankani <anuragnankani3@gmail.com>
2025-09-02 21:44:23 +0530

    show data and validations

--- JAVASCRIPT/Crud-App/overAll.js
+++ JAVASCRIPT/Crud-App/overAll.js
@@ -1,7 +1,7 @@
-
+function createRandomId(){
+return Date.now().toString(36) +  Math.random().toString(36).substr(2,8);
+}
 function handelSubmit(){
-    
-
    let firstName = document.getElementById('firstName').value;
    let lastName = document.getElementById('lastName').value;
    let userName = document.getElementById('userName').value;
@@ -9,12 +9,20 @@ function handelSubmit(){
    let mobile = document.getElementById('mobile').value;
    let gender = document.getElementById('gender').value;
    let password = document.getElementById('password').value;
-   console.log(firstName)
-   debugger
+   
+   let allData = JSON.parse(localStorage.getItem('records')) || [];
+   let compareEmail = allData.some((value)=> value.email === emailAddress );
+   console.log(compareEmail)
+   
+   if(compareEmail){
+   return alert('Email Id is already Registered')
+   }
 
-  
-
-    const obj ={
+   if(!firstName || !lastName || !userName || !emailAddress || !mobile || !gender || !password){
+   return alert("All Fields are Required")
+   }
+     const obj ={
+        id: createRandomId(),
         firstName:firstName,
         lastName: lastName,
         userName: userName,
@@ -23,13 +31,55 @@ function handelSubmit(){
         email: emailAddress,
         password: password
     };
- let allData = JSON.parse(localStorage.getItem('read'));
+
 
     allData.push(obj);
 
     localStorage.setItem('records', JSON.stringify(allData));
+   }   
+console.log('js file');
 
+
+function showMyData(){
+    let allData = JSON.parse(localStorage.getItem('records')) || [];
+    let printData = document.getElementById('tableBody');
+    let showData = allData.map((value, index)=>{
+                  return  `
+                    <tr>
+                    <td>  ${ index + 1 } </td>  
+                    <td> ${value.firstName}  </td>  
+                    <td> ${value.lastName}  </td>  
+                    <td> ${value.userName}  </td>  
+                    <td> ${value.email}  </td>  
+                    <td> ${value.mobile}  </td>  
+                    <td> ${value.gender}  </td>  
+                    <td> ${value.password}  </td>  
+                  <td>  
+                  <button> Edit</button> 
+                  <button onclick={handelDelete('${index}')}  > Delete</button> 
+                  </td>
+                    </tr>
+                    `
+    })
+    printData.innerHTML = showData.join("")
 }
 
 
-console.log('js file');
+function handelDelete(index){
+console.log('index', index);
+let allData = JSON.parse(localStorage.getItem('records')) || [];
+if(!index){
+   return alert('Index is Required')
+}
+debugger
+if(index !== -1){
+    allData.splice(index,1)
+
+    localStorage.setItem("records",JSON.stringify(allData));
+    location.reload()
+}
+}
+
+showMyData()
+
+
